af8d2a90528114858a89a491acc0cb961f25ecc3
RizalR117 <rizal.rudiansyah11@gmail.com>
2023-04-12 12:10:56 +0700

    lessson 1

---
+++ android/app/src/main/kotlin/com/example/rizal2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rizal2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel{
+  int id;
+  String codeName;
+  String name;
+  String major;
+
+  UserModel({
+    required this.id,
+    required this.codeName,
+    required this.name,
+    required this.major,
+  });
+}
